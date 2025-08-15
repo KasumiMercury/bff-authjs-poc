27e7f720afa3245fa92ce133a36c1c4dddfccf6f
KasumiMercury <naoki_shira@outlook.jp>
2025-08-15 17:51:13 +0900

    fix

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -218,6 +218,8 @@ export default function Login() {
                     name="email"
                     type="email"
                     required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="メールアドレス"
                   />
